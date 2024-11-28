119534230d5c477340a66f91acbf04b6cd08e4c1
BKarpow <xymerone@gmail.com>
2024-11-28 17:42:33 +0200

    1.1.1

--- src/WorkUP.h
+++ src/WorkUP.h
@@ -6,10 +6,12 @@ class WorkUP {
   private:
     uint32_t _up;
     uint32_t _tmr;
+    uint16_t _tmrDelay;
     uint32_t _periodTimer;
     uint16_t _speed;
     bool _run;
     bool _pause;
+    bool _millisMode = false;
 
     String addSymbNull(uint8_t t) {
       String s = "";
@@ -19,11 +21,11 @@ class WorkUP {
     }
 
     uint32_t divideBy24(uint32_t x) {
-        return (x * 178956970u) >> 32; // Множник для ділення на 24
+        return x / 24; // Множник для ділення на 24
     }
 
     uint32_t divideBy60(uint32_t x) {
-        return (x * 0x8889) >> 19; // Швидке ділення на 60
+        return x / 60; // Швидке ділення на 60
     }
 
     uint32_t remainderBy60(uint32_t x) {
@@ -35,21 +37,26 @@ class WorkUP {
         uint32_t quotient = divideBy24(x);
         return x - (quotient * 24); // Залишок від ділення
     }
+
+    uint32_t _getUp()
+    {
+      return (_millisMode) ? millis() / 1000 : _up;
+    }
   public:
     WorkUP() {
       _speed = 1;
       _periodTimer = 1000;
     }
 
-    void setSpeed(uint8_t speed) {
-      _speed = speed;
+    void useMillisTimeAsUptime() // Max 24 days Uptime !!!!
+    {
+      _millisMode = true;
     }
 
     void begin() {
       _run = true;
       _pause = false;
       _up = 0;
-      _speed = 1;
     }
 
     void end() {
@@ -67,35 +74,35 @@ class WorkUP {
     }
 
     uint8_t getSecunds() {
-      return remainderBy60(_up);
+      return (uint8_t)remainderBy60(_getUp());
     }
 
     uint8_t getMinutes() {
-      uint32_t res = divideBy60(_up);
+      uint32_t res = divideBy60(_getUp());
       res = remainderBy60(res);
       return (uint8_t)res;
     }
 
     uint8_t getHours() {
-      uint32_t res = divideBy60(_up);
+      uint32_t res = divideBy60(_getUp());
       res = divideBy60(res);
       res = remainderBy24(res);
       return (uint8_t)res;
     }
 
     uint16_t getDays() {
-      uint32_t res = divideBy60(_up);
+      uint32_t res = divideBy60(_getUp());
       res = divideBy60(res);
       res = divideBy24(res);
       return (uint16_t)res;
     }
 
     uint32_t getUpSecunds() {
-      return _up;
+      return _getUp();
     }
 
     uint32_t getUpMinutes() {
-      return divideBy60(_up);
+      return divideBy60(_getUp());
     }
 
     uint32_t getUpHours() {
@@ -122,14 +129,24 @@ class WorkUP {
     }
 
     void tick() {
-      if (millis() - _tmr > _periodTimer) {
-        // _tmr = millis();
+      if (_millisMode) return;
+      if (millis() - _tmr >= _periodTimer) {
         _up += _speed;
         do {
           _tmr += _periodTimer;
           if (_tmr < _periodTimer) break;
         } while (_tmr < millis() - _periodTimer);
       } 
+    }
+
+    void delayAsync1Sec(void (*_callback)())
+    {
+      uint16_t timerLeft = millis() - _tmrDelay;
+      uint16_t prdDelay1 = 1024;
+      if (timerLeft >= prdDelay1) {
+        _tmrDelay += prdDelay1 * (timerLeft >> 10);
+        _callback();
+      }
     }
 };
 
